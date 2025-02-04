d244f6ee1e076229eb9748b952d2efd8db0a88be
HarentsoaMichou <harentsoamichou@gmail.com>
2025-02-04 08:58:56 +0300

    Accueil
    Base_url

--- app/views/M_accueil.php
+++ app/views/M_accueil.php
@@ -10,27 +10,27 @@
   <meta content="" name="keywords">
 
   <!-- Favicons -->
-  <link href="assets/img/logo2.webp" rel="icon">
-  <link href="assets/img/apple-touch-icon.png" rel="apple-touch-icon">
+  <link href="<?= BASE_URL ?>public/assets/img/logo2.webp" rel="icon">
+  <link href="<?= BASE_URL ?>public/assets/img/apple-touch-icon.png" rel="apple-touch-icon">
 
   
   <!-- Template Main CSS File -->
-  <link href="assets/css/style.css" rel="stylesheet">
+  <link href="<?= BASE_URL ?>public/assets/css/style.css" rel="stylesheet">
 
   <!-- Vendor CSS Files -->
-  <link href="assets/vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
-  <link href="assets/vendor/bootstrap-icons/bootstrap-icons.css" rel="stylesheet">
-  <link href="assets/vendor/boxicons/css/boxicons.min.css" rel="stylesheet">
-  <link href="assets/vendor/quill/quill.snow.css" rel="stylesheet">
-  <link href="assets/vendor/quill/quill.bubble.css" rel="stylesheet">
-  <link href="assets/vendor/remixicon/remixicon.css" rel="stylesheet">
-  <link href="assets/vendor/simple-datatables/style.css" rel="stylesheet">
-  <script src="assets/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+  <link href="<?= BASE_URL ?>public/assets/vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+  <link href="<?= BASE_URL ?>public/assets/vendor/bootstrap-icons/bootstrap-icons.css" rel="stylesheet">
+  <link href="<?= BASE_URL ?>public/assets/vendor/boxicons/css/boxicons.min.css" rel="stylesheet">
+  <link href="<?= BASE_URL ?>public/assets/vendor/quill/quill.snow.css" rel="stylesheet">
+  <link href="<?= BASE_URL ?>public/assets/vendor/quill/quill.bubble.css" rel="stylesheet">
+  <link href="<?= BASE_URL ?>public/assets/vendor/remixicon/remixicon.css" rel="stylesheet">
+  <link href="<?= BASE_URL ?>public/assets/vendor/simple-datatables/style.css" rel="stylesheet">
+  <script src="<?= BASE_URL ?>public/assets/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
 
   <!-- Bootstrap -->
-  <link rel="stylesheet" href="assets/Bootstrap/css/bootstrap.min.css">
-  <script src="assets/Bootstrap/js/bootstrap.min.js"></script>
-  <script src="assets/Bootstrap/js/jquery.min.js"></script>
+  <link rel="stylesheet" href="<?= BASE_URL ?>public/assets/Bootstrap/css/bootstrap.min.css">
+  <script src="<?= BASE_URL ?>public/assets/Bootstrap/js/bootstrap.min.js"></script>
+  <script src="<?= BASE_URL ?>public/assets/Bootstrap/js/jquery.min.js"></script>
 
 
 </head>
@@ -42,7 +42,7 @@
 
     <div class="d-flex align-items-center justify-content-between">
       <a href="index.html" class="logo d-flex align-items-center">
-        <img src="assets/img/logo2.webp" alt="">
+        <img src="<?= BASE_URL ?>public/assets/img/logo2.webp" alt="">
         <span class="d-none d-lg-block">FarmFrenzy</span>
       </a>
       <i class="bi bi-list toggle-sidebar-btn"></i>
@@ -217,18 +217,18 @@
   <a href="#" class="back-to-top d-flex align-items-center justify-content-center"><i class="bi bi-arrow-up-short"></i></a>
 
   <!-- Vendor JS Files -->
-  <script src="assets/vendor/apexcharts/apexcharts.min.js"></script>
-  <script src="assets/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
-  <script src="assets/vendor/chart.js/chart.umd.js"></script>
-  <script src="assets/vendor/echarts/echarts.min.js"></script>
-  <script src="assets/vendor/quill/quill.js"></script>
-  <script src="assets/vendor/simple-datatables/simple-datatables.js"></script>
-  <script src="assets/vendor/tinymce/tinymce.min.js"></script>
-  <script src="assets/vendor/php-email-form/validate.js"></script>
+  <script src="<?= BASE_URL ?>public/assets/vendor/apexcharts/apexcharts.min.js"></script>
+  <script src="<?= BASE_URL ?>public/assets/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+  <script src="<?= BASE_URL ?>public/assets/vendor/chart.js/chart.umd.js"></script>
+  <script src="<?= BASE_URL ?>public/assets/vendor/echarts/echarts.min.js"></script>
+  <script src="<?= BASE_URL ?>public/assets/vendor/quill/quill.js"></script>
+  <script src="<?= BASE_URL ?>public/assets/vendor/simple-datatables/simple-datatables.js"></script>
+  <script src="<?= BASE_URL ?>public/assets/vendor/tinymce/tinymce.min.js"></script>
+  <script src="<?= BASE_URL ?>public/assets/vendor/php-email-form/validate.js"></script>
 
   
   <!-- Template Main JS File -->
-  <script src="assets/js/main.js"></script>
+  <script src="<?= BASE_URL ?>public/assets/js/main.js"></script>
 
 </body>
 
